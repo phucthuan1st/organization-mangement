822d8847b95b4cd56b1c163e2e5a866620338431
Fidisk <70708171+Fidisk@users.noreply.github.com>
2023-11-16 01:19:31 +0700

    Login form modified
    - Proper login switch state
    - Small red text for invalid password or user name
    
    For testing:
    Account: user1,user2
    Password: pass1,pass2

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,26 +3,78 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebookF, faGoogle, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
 import './Login.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+//import { View, Image } from 'react-native';
 
 library.add(faFacebookF, faTwitter, faLinkedinIn, faGoogle);
 
-
 const LoginSection = () => {
-  return (
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+
+const handleClick = (event) => {
+  //Prevent page reload
+  event.preventDefault();
+
+  var { uname, pass } = document.forms[0];
+
+  // Find user login info
+  const userData = database.find((user) => user.username === uname.value);
+
+  // Compare user info
+  if (userData) {
+    if (userData.password !== pass.value) {
+      // Invalid password
+      setErrorMessages({ name: "pass", message: errors.pass });
+    } else {
+      setIsSubmitted(true);
+    }
+  } else {
+    // Username not found
+    setErrorMessages({ name: "uname", message: errors.uname });
+  }
+};
+
+  const renderErrorMessage = (name) =>
+  name === errorMessages.name && (
+    <div className="error">{errorMessages.message}</div>
+  );
+
+  const RenderTab = (
     <section className="vh-100">
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
             <img 
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              //src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              src='http://www.chemistry.hcmus.edu.vn/images/logo%20KHTN_REMAKE%201.png'
               className="img-fluid" 
               alt="Sample img"
             />
           </div>
           <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <form>
+            <form onClick={handleClick}>
               <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                 <p className="lead fw-normal mb-0 me-3">Sign in with</p>
                 <button type="button" className="btn btn-primary btn-floating mx-1">
@@ -41,19 +93,25 @@ const LoginSection = () => {
               <div className="divider d-flex align-items-center my-4">
                 <p className="text-center fw-bold mx-3 mb-0">Or</p>
               </div>
-
+              
               {/* Email input */}
               <div className="form-outline mb-4">
                 <label className="form-label" htmlFor="form3Example3">Email address</label>
-                <input type="email" id="form3Example3" className="form-control form-control-lg"
+                {/*
+                  <input type="email" id="form3Example3" className="form-control form-control-lg"
                   placeholder="Enter a valid email address" />
+                */}
+                  <input type="email" name="uname" id="form3Example3" className="form-control form-control-lg"
+                    placeholder="Enter a valid email address" required/>
+                  {renderErrorMessage("uname")}
               </div>
 
               {/* Password input */}
               <div className="form-outline mb-3">
                 <label className="form-label" htmlFor="form3Example4">Password</label>
                 <input type="password" id="form3Example4" className="form-control form-control-lg"
-                  placeholder="Enter password" />
+                  placeholder="Enter password" name="pass" required />
+                {renderErrorMessage("pass")}
               </div>
 
               <div className="d-flex justify-content-between align-items-center">
@@ -69,7 +127,9 @@ const LoginSection = () => {
 
               <div className="text-center text-lg-start mt-4 pt-2">
                 <button type="button" className="btn btn-primary btn-lg"
-                  style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>Login</button>
+                  style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>Login
+
+                </button>
                 <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
                     className="link-danger">Register</a></p>
               </div>
@@ -105,6 +165,12 @@ const LoginSection = () => {
       </div>
     </section>
   );
+
+  return (
+    <div className="loginForm">
+      {isSubmitted ? <div> {/* Do something*/} </div> : RenderTab}
+    </div>
+  )
 };
 
 export default LoginSection;
