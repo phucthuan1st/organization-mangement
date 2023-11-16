3661624b7631e08ce86218003190d7fcd5fe582b
phucthuan1st <phucthuan1st@gmail.com>
2023-11-16 12:06:37 +0700

    properly handle field input and validation - use onBlur for field focus and focus out - use onClick for form submit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,15 @@
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebookF, faGoogle, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import 'bootstrap/dist/css/bootstrap.css';
-import './Login.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookF,
+  faGoogle,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "bootstrap/dist/css/bootstrap.css";
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import "./Login.css";
+//import ReactDOM from "react-dom";
 //import { View, Image } from 'react-native';
 
 library.add(faFacebookF, faTwitter, faLinkedinIn, faGoogle);
@@ -19,57 +23,77 @@ const LoginSection = () => {
   const database = [
     {
       username: "user1",
-      password: "pass1"
+      password: "pass1",
     },
     {
       username: "user2",
-      password: "pass2"
-    }
+      password: "pass2",
+    },
   ];
 
   const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
+    not_exist_uname: "Username does not exist. Please register first.",
+    wrong_password: "Incorrect password.",
+    blank_field: "Please fill in the required fields.",
   };
 
-
-const handleClick = (event) => {
-  //Prevent page reload
-  event.preventDefault();
-
-  var { uname, pass } = document.forms[0];
-
-  // Find user login info
-  const userData = database.find((user) => user.username === uname.value);
-
-  // Compare user info
-  if (userData) {
-    if (userData.password !== pass.value) {
-      // Invalid password
-      setErrorMessages({ name: "pass", message: errors.pass });
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+  
+    // Check if the field is blank
+    if (!value) {
+      setErrorMessages({ name: `${name}_blank`, message: errors.blank_field });
     } else {
-      setIsSubmitted(true);
+      // Clear the error message if the field is not blank
+      setErrorMessages({});
     }
-  } else {
-    // Username not found
-    setErrorMessages({ name: "uname", message: errors.uname });
-  }
-};
-
+  };
+  
+  const handleClick = (event) => {
+    event.preventDefault();
+  
+    const { uname, pass } = document.forms[0];
+  
+    // Check if any field is blank on button click
+    if (!uname.value || !pass.value) {
+      setErrorMessages({ name: "blank", message: errors.blank_field });
+      return;
+    }
+  
+    // TODO: implement authentication process here
+    // Below is for testing only
+    const userData = database.find((user) => user.username === uname.value);
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({
+          name: "wrong_pass",
+          message: errors.wrong_password,
+        });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({
+        name: "not_exist_uname",
+        message: errors.not_exist_uname,
+      });
+    }
+  };
+  
   const renderErrorMessage = (name) =>
-  name === errorMessages.name && (
-    <div className="error">{errorMessages.message}</div>
-  );
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
 
   const RenderTab = (
     <section className="vh-100">
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
-            <img 
+            <img
               //src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
               src='http://www.chemistry.hcmus.edu.vn/images/logo%20KHTN_REMAKE%201.png'
-              className="img-fluid" 
+              className="img-fluid"
               alt="Sample img"
             />
           </div>
@@ -77,15 +101,24 @@ const handleClick = (event) => {
             <form onClick={handleClick}>
               <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                 <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-                <button type="button" className="btn btn-primary btn-floating mx-1">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-floating mx-1"
+                >
                   <FontAwesomeIcon icon={faFacebookF} />
                 </button>
 
-                <button type="button" className="btn btn-primary btn-floating mx-1">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-floating mx-1"
+                >
                   <FontAwesomeIcon icon={faTwitter} />
                 </button>
 
-                <button type="button" className="btn btn-primary btn-floating mx-1">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-floating mx-1"
+                >
                   <FontAwesomeIcon icon={faLinkedinIn} />
                 </button>
               </div>
@@ -93,56 +126,86 @@ const handleClick = (event) => {
               <div className="divider d-flex align-items-center my-4">
                 <p className="text-center fw-bold mx-3 mb-0">Or</p>
               </div>
-              
+
               {/* Email input */}
               <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="form3Example3">Email address</label>
-                {/*
-                  <input type="email" id="form3Example3" className="form-control form-control-lg"
-                  placeholder="Enter a valid email address" />
-                */}
-                  <input type="email" name="uname" id="form3Example3" className="form-control form-control-lg"
-                    placeholder="Enter a valid email address" required/>
-                  {renderErrorMessage("uname")}
+                <label className="form-label" htmlFor="form3Example3">
+                  Email address
+                </label>
+
+                <input
+                  type="email"
+                  name="uname"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  onBlur={handleBlur}
+                  required
+                />
+                {renderErrorMessage("uname_blank")}
+                {renderErrorMessage("not_exist_uname")}
               </div>
 
               {/* Password input */}
               <div className="form-outline mb-3">
-                <label className="form-label" htmlFor="form3Example4">Password</label>
-                <input type="password" id="form3Example4" className="form-control form-control-lg"
-                  placeholder="Enter password" name="pass" required />
-                {renderErrorMessage("pass")}
+                <label className="form-label" htmlFor="form3Example4">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  name="pass"
+                  onBlur={handleBlur}
+                  required
+                />
+                {renderErrorMessage("pass_blank")}
+                {renderErrorMessage("wrong_pass")}
               </div>
 
               <div className="d-flex justify-content-between align-items-center">
                 {/* Checkbox */}
                 <div className="form-check mb-0">
-                  <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                  <input
+                    className="form-check-input me-2"
+                    type="checkbox"
+                    value=""
+                    id="form2Example3"
+                  />
                   <label className="form-check-label" htmlFor="form2Example3">
                     Remember me
                   </label>
                 </div>
-                <a href="#!" className="text-body">Forgot password?</a>
+                <a href="#!" className="text-body">
+                  Forgot password?
+                </a>
               </div>
 
               <div className="text-center text-lg-start mt-4 pt-2">
-                <button type="button" className="btn btn-primary btn-lg"
-                  style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>Login
-
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg"
+                  style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                  onClick={handleClick}
+                >
+                  Login
                 </button>
-                <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                    className="link-danger">Register</a></p>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Don't have an account?{" "}
+                  <a href="#!" className="link-danger">
+                    Register
+                  </a>
+                </p>
               </div>
-
             </form>
           </div>
         </div>
       </div>
-      <div
-        className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+      <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
         {/* Copyright */}
         <div className="text-white mb-3 mb-md-0">
-          Copyright © 2020. All rights reserved.
+          Copyright © 2023. All rights reserved.
         </div>
         {/* Copyright */}
 
@@ -170,7 +233,7 @@ const handleClick = (event) => {
     <div className="loginForm">
       {isSubmitted ? <div> {/* Do something*/} </div> : RenderTab}
     </div>
-  )
+  );
 };
 
 export default LoginSection;
