ab307b8dfbc603c7951326e2da2e4674de1f356e
DANNOSIUCUTE <147748745+DANNOSIUCUTE@users.noreply.github.com>
2023-12-22 20:22:15 +0700

    Update Calendar.js

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Calendar } from "react-calendar"; // Import your chosen calendar library
-import "./Calendar.css"
+import { Calendar } from "react-calendar";
+import "./Calendar.css";
 
 const CalendarTab = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -11,13 +11,13 @@ const CalendarTab = () => {
     // Replace this with your actual event fetching logic
     setEvents([
       {
-        title: "Meeting with John",
-        date: new Date("2023-12-15"),
+        title: "Kiểm điểm đánh giá cuối năm",
+        date: new Date("2023-12-24"),
         hasEvent: true,
       },
       {
-        title: "Project deadline",
-        date: new Date("2023-12-20"),
+        title: "Demo sản phẩm",
+        date: new Date("2023-12-23"),
         hasEvent: true,
       },
     ]);
@@ -25,25 +25,38 @@ const CalendarTab = () => {
 
   const handleDateChange = (date) => setSelectedDate(date);
 
-  const filteredEvents = events.filter(
-    (event) => event.date.getFullYear() === selectedDate.getFullYear() &&
-      event.date.getMonth() === selectedDate.getMonth() &&
-      event.date.getDate() === selectedDate.getDate()
-  );
+  const tileClassName = ({ date }) => {
+    const hasEvent = events.some(
+      (event) =>
+        event.date.getFullYear() === date.getFullYear() &&
+        event.date.getMonth() === date.getMonth() &&
+        event.date.getDate() === date.getDate()
+    );
+
+    return hasEvent ? "has-event" : null;
+  };
 
   return (
     <div className="calendar-tab">
       <Calendar
         onChange={handleDateChange}
         value={selectedDate}
+        tileClassName={tileClassName}
       />
       <div className="event-list">
         <h3>Events for {selectedDate.toLocaleDateString()}:</h3>
-        {filteredEvents.map((event) => (
-          <div className="event-card" key={event.title}>
-            <h4>{event.title}</h4>
-          </div>
-        ))}
+        {events
+          .filter(
+            (event) =>
+              event.date.getFullYear() === selectedDate.getFullYear() &&
+              event.date.getMonth() === selectedDate.getMonth() &&
+              event.date.getDate() === selectedDate.getDate()
+          )
+          .map((event) => (
+            <div className="event-card" key={event.title}>
+              <h4>{event.title}</h4>
+            </div>
+          ))}
       </div>
     </div>
   );
